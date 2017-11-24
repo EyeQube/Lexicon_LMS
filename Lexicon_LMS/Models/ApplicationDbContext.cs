846a9bf3b6ec3ca4763357af8ed0b81248bcf157
Sattar <User@DESK-FG2GJ32>
2017-11-24 11:38:50 +0100

    Added_FirstAndLastNameToApplicationUser

---
+++ Lexicon_LMS/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Lexicon_LMS.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
