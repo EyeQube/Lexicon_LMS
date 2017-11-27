890e6d503b42fca02d623de14563ff18cefb33e1
Gunnar <gunnar.rydberg@hotmail.com>
2017-11-27 11:26:58 +0100

    Add seed for courses and link to users

--- Lexicon_LMS/App_Start/IdentityConfig.cs
+++ Lexicon_LMS/App_Start/IdentityConfig.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
+﻿using Lexicon_LMS.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using Lexicon_LMS.Models;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Lexicon_LMS
 {
@@ -40,7 +36,7 @@ namespace Lexicon_LMS
         {
         }
 
-        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
         {
             var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
             // Configure validation logic for usernames
@@ -53,11 +49,19 @@ namespace Lexicon_LMS
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
+#if DEBUG
+                RequiredLength = 0,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
+#else
                 RequiredLength = 6,
                 RequireNonLetterOrDigit = true,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
+#endif
             };
 
             // Configure user lockout defaults
@@ -81,7 +85,7 @@ namespace Lexicon_LMS
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
             {
-                manager.UserTokenProvider = 
+                manager.UserTokenProvider =
                     new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
             }
             return manager;
