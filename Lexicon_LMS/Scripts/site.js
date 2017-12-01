8abc8a41195499e1d98cf32e4422c749312cf06e
Gunnar <gunnar.rydberg@hotmail.com>
2017-12-01 16:12:15 +0100

    refactor: general script to delete users

--- Lexicon_LMS/Scripts/site.js
+++ Lexicon_LMS/Scripts/site.js
@@ -1,49 +1,11 @@
 ﻿$(document).ready(function () {
     $('.datepickerDate').datepicker({ format: 'yyyy/mm/dd', autoclose: true, locale: 'sw' });
-
-    $('.js-delete-user').click(function (e) {
-        var link = $(e.target);
-
-        bootbox.dialog({
-            title: 'Confirm',
-            message: "Are you sure you want to delete this user?",
-            buttons: {
-                no: {
-                    label: "No",
-                    className: 'btn-default',
-                    callback: function () {
-                        bootbox.hideAll();
-                    }
-                },
-                yes: {
-                    label: "Yes",
-                    className: 'btn-danger',
-                    callback: function () {
-                        $.ajax({
-                            url: "/api/Account/" + link.attr("data-user-id"),
-                            method: "DELETE"
-                        })
-                            .done(function () {
-                                link.parents("tr").fadeOut(function () {
-                                    $(this).remove();
-                                });
-                            })
-                            .fail(function () {
-                                alert("Something failed!!");
-                            });
-
-                    }
-                }
-            }
-        });
-
-    });
     
     // General function for removing stuff in database and DOM.
     // Uses the following html data attributes:
     //    data-user-api:    Database API for removing something
-    //    data-user-id:     Id for API
-    //    data-user-remove: DOM element to remove on success
+    //    data-user-id:     Id for API (ie. /controller/action/{id} )
+    //    data-user-remove: DOM element to remove on success (ie. a css class descriptor)
     //    data-user-message:Text to show in confirmation box
     //
     $('.js-delete-entity').click(function (e) {
