a987d52780bdd0fe1234b235adb00e68b571e0a3
Gunnar <gunnar.rydberg@hotmail.com>
2017-12-04 11:34:03 +0100

    add save-and-add-another to user register form

--- Lexicon_LMS/Controllers/AccountController.cs
+++ Lexicon_LMS/Controllers/AccountController.cs
@@ -152,7 +152,7 @@ namespace Lexicon_LMS.Controllers
         [HttpPost]
         [Authorize(Roles = Role.Teacher)]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel viewModel)
+        public async Task<ActionResult> Register(RegisterViewModel viewModel, string button)
         {
 
             if (ModelState.IsValid)
@@ -165,6 +165,12 @@ namespace Lexicon_LMS.Controllers
                 if (result.Succeeded)
                 {
                     UserManager.AddToRole(user.Id, viewModel.Role);
+
+                    if (button == "SaveNew")
+                    {
+                        viewModel.Courses = _context.Courses;
+                        return View(viewModel);
+                    }
 
                     return RedirectToAction("Index", "Home");
                 }
