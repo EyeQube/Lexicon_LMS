8985ae12efac278b697394090a8218371c9b1e4f
Gunnar <gunnar.rydberg@hotmail.com>
2017-12-08 11:32:12 +0100

    add links for students to upload files

--- Lexicon_LMS/Controllers/DocumentController.cs
+++ Lexicon_LMS/Controllers/DocumentController.cs
@@ -1,6 +1,8 @@
 ﻿using Lexicon_LMS.Models;
+using Microsoft.AspNet.Identity;
 using System;
 using System.IO;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -34,6 +36,19 @@ namespace Lexicon_LMS.Controllers
 
             if (ModelState.IsValid)
             {
+                var userId = User.Identity.GetUserId();
+                // Validate user rights:
+                if (User.IsInRole(Role.Student))
+                {
+                    // Activity must allow studend assignments and student must be enrolled in course
+                    var courseId = db.Activities.Find(document.ActivityId).Module.Course.Id;
+                    if (db.Users.Find(userId).CourseId != courseId)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Student not allowed to upload in this course (student not enrolled in specified course");
+                    }
+                }
+                document.Author = db.Users.Find(userId);
+
                 db.Documents.Add(document);
                 db.SaveChanges();
                 var rootPath = AppDomain.CurrentDomain.BaseDirectory;
