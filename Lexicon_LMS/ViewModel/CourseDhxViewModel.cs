84629630ba282e21d61b50943cdbeff1ce745f89
Sattar <sattar.alvandpour@gmail.com>
2017-12-27 22:56:50 +0100

    Refactorized CourseDhxViewModel

--- Lexicon_LMS/ViewModel/CourseDhxViewModel.cs
+++ Lexicon_LMS/ViewModel/CourseDhxViewModel.cs
@@ -15,27 +15,6 @@ namespace Lexicon_LMS.ViewModel
         public Course Course { get; set; }
         public DHXScheduler DHX { get; set; }
 
-        public string text { get; set; }
-        public DateTime start_date { get; set; }
-        public DateTime end_date { get; set; }
-
-        public int CourseId { get; set; }
-
-
-        public CourseDhxViewModel()
-        {
-
-        }
-
-        public CourseDhxViewModel(Course course)
-        {
-
-            text = course.Description;
-            start_date = new DateTime();
-            end_date = new DateTime();
-            CourseId = course.Id;
-
-        }
 
     }
 }
